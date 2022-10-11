7cc11a01d92421c874722565ef593f3be71e3e2a
Olena Lopatina <106389715+aknelua@users.noreply.github.com>
2022-10-12 00:52:55 +0200

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,30 @@
+const langArr = {
+	"button_About": {
+		"en": "About",
+		"ua": "Опис",
+	},
+	"button_Portfilio": {
+		"en": "Portfilio",
+		"ua": "Портфоліо",
+	},
+	"button_Education": {
+		"en": "Education",
+		"ua": "Освіта",
+	},
+	"about_title": {
+		"en": "About",
+		"ua": "Про мене",
+	},
+	"about_text1": {
+		"en": "Hi, I`m Olena Lopatina, freelancer, web deweloper.",
+		"ua": "Привіт, я Олена Лопатіна, фрілансер, веб-розробник. ",
+	},
+	"about_text2": {
+		"en": "I don`t have huge experience in web development, but here I want you to see what I can . I do  modern responsive website using HTML, CSS and JavaScript.",
+		"ua": "У мене немає великого досвіду веб-розробки, але тут я хочу, щоб ви побачили, мої навички. Я створюю сучасний адаптивний веб-сайт, використовуючи HTML, CSS та JavaScript.",
+	},	
+	"portfolio_title": {
+		"en": "Portfolio",
+		"ua": "Портфоліо",
+	},	
+}
---
+++ js/script.js
@@ -0,0 +1,45 @@
+const select = document.querySelector('select');
+const allLang = ['en', 'ua'];
+select.addEventListener('change', changeURLLanguage);
+function changeURLLanguage() {
+	let lang = select.value;
+	location.href = window.location.pathname + '#' + lang;
+	location.reload();
+}
+function changeLenguege() {
+	let hash = window.location.hash;
+	hash = hash.substring(1);
+	if (!allLang.includes(hash)) {
+		location.href = window.location.pathname + '#en';
+		location.reload();
+	}
+	select.value = hash;
+	for (let key in langArr) {
+		let elem = document.querySelector('.lng-' + key);
+		if (elem) {
+			elem.innerHTML = langArr[key][hash];
+		}
+	}
+}
+changeLenguege();
+
+const smoothLinks = document.querySelectorAll("a[href^='#']");
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute("href");
+
+        document.querySelector(id).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+};
+
+//const sloganReload = document.querySelector("intro_slogan");
+// sloganReload.onclick = function () {
+// 	location.reload();
+// }
+// sloganReload.addEventListener("click", , function () {
+// 	location.reload()
+// });
