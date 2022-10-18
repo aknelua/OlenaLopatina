59a03cdb19151ea2684bc53ff1ebff627de5e86b
Olena Lopatina <106389715+aknelua@users.noreply.github.com>
2022-10-18 20:45:14 +0200

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -20,8 +20,8 @@ const langArr = {
 		"ua": "Привіт, я Олена Лопатіна, фрілансер, веб-розробник. ",
 	},
 	"about_text2": {
-		"en": "I don`t have huge experience in web development, but here I want you to see what I can . I do  modern responsive website using <span class='bold'>HTML, CSS</span> and <span class='bold'>JavaScript</span>.",
-		"ua": "У мене немає великого досвіду веб-розробки, але тут я хочу, щоб ви побачили, мої навички. Я створюю сучасний адаптивний веб-сайт, використовуючи <span class='bold'>HTML, CSS</span> та <span class='bold'>JavaScript</span>.",
+		"en": "I don`t have huge experience in web development, but here I want you to see what I can . I do  modern, responsive, user-friendly website using <span class='bold'>HTML, CSS</span> and <span class='bold'>JavaScript</span>.",
+		"ua": "У мене немає великого досвіду веб-розробки, але тут я хочу, щоб ви побачили, мої навички. Я створюю сучасний, зручний, адаптивний веб-сайт, використовуючи <span class='bold'>HTML, CSS</span> та <span class='bold'>JavaScript</span>.",
 	},	
 	"portfolio_title": {
 		"en": "Portfolio",
